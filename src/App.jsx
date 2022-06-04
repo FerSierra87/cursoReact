9b7aaa8addc30489c21a23df3881d751273f0edf
JORGE SIERRA <jorge.sierra@estudiantes.utec.edu.uy>
2022-06-03 23:40:54 -0300

    Agregando NavBar

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import Cuerpo from './components/Cuerpo';
+import NavBar from './components/NavBar';
+function App() {
+  return (
+    <>
+    <NavBar />
+    <Cuerpo />
+    </>
+  );
+}
+
+export default App;
